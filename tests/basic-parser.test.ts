4d3a88881f9906ff79b07febae3dd6905de7c7e4
Yevhen_Burkovskyi <yevhen_burkovskyi@brown.edu>
2025-09-13 11:21:46 -0400

    feat: added testing databases and tests themselves

--- tests/basic-parser.test.ts
+++ tests/basic-parser.test.ts
@@ -2,6 +2,10 @@ import { parseCSV } from "../src/basic-parser";
 import * as path from "path";
 
 const PEOPLE_CSV_PATH = path.join(__dirname, "../data/people.csv");
+const SHAKESPEAR_CSV_PATH = path.join(__dirname, "../data/shakespear.csv");
+const ALGORITHMS_CSV_PATH = path.join(__dirname, "../data/algorithms.csv");
+const ALLONONELINE_CSV_PATH = path.join(__dirname, "../data/allOnOneLine.csv");
+const UNEQUAL_CSV_PATH = path.join(__dirname, "../data/unequal.csv");
 
 test("parseCSV yields arrays", async () => {
   const results = await parseCSV(PEOPLE_CSV_PATH)
@@ -14,9 +18,59 @@ test("parseCSV yields arrays", async () => {
   expect(results[4]).toEqual(["Nim", "22"]);
 });
 
+test("parseCSV returns correct types:strings", async () => {
+  const results = await parseCSV(PEOPLE_CSV_PATH)
+  
+  expect(results[0]).toEqual(["name", "age"]);
+  expect(results[1]).toEqual(["Alice", "23"]);
+  expect(results[2]).toEqual(["Bob", "thirty"]);
+  expect(results[3]).toEqual(["Charlie", "25"]);
+  expect(results[4]).toEqual(["Nim", "22"]);
+});
+
+
+
 test("parseCSV yields only arrays", async () => {
   const results = await parseCSV(PEOPLE_CSV_PATH)
   for(const row of results) {
     expect(Array.isArray(row)).toBe(true);
   }
 });
+
+test("parceCSV inner arrays on return have equal length, or error fires", async () => {
+  const results_1 = await parseCSV(SHAKESPEAR_CSV_PATH)
+  const results_2 = await parseCSV(ALGORITHMS_CSV_PATH)
+  const results_3 = await parseCSV(UNEQUAL_CSV_PATH)
+  for(let i=0; i<results_1.length - 2; i++){
+    expect(results_1[i].length).toEqual(results_1[i+1].length)
+  }
+  for(let i=0; i<results_2.length - 2; i++){
+    expect(results_2[i].length).toEqual(results_2[i+1].length)
+  } 
+});
+
+test("parceCSV will always return correct length", async () => {
+  const results_1 = await parseCSV(SHAKESPEAR_CSV_PATH)
+  const results_2 = await parseCSV(ALGORITHMS_CSV_PATH)
+  const results_3 = await parseCSV(ALLONONELINE_CSV_PATH)
+  expect(results_1).toHaveLength(5);
+  expect(results_2).toHaveLength(4);
+  expect(results_3).toHaveLength(1);
+});
+
+
+test("parseCSV returns correct types: number", async () => {
+  const results = await parseCSV(ALGORITHMS_CSV_PATH)
+  expect(results[1][2] + 3).toEqual(1959);
+  expect(results[2][2] + 100).toEqual(2026);
+});
+
+test("parceCSV will not separate commas in double quotes", async () => {
+  const results = await parseCSV(SHAKESPEAR_CSV_PATH)
+  expect(results[2]).toEqual(["Julius Ceasar", "“The fault, dear Brutus, is not in our stars, but in ourselves.”", 9114])
+  expect(results[4]).toEqual(["The Winter's Tale", "“Exit, pursued by a bear”", 33670])
+});
+
+
+
+
